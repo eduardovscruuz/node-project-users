0b5d082b3d5506a34485ab4903a8a6ce31671d83
eduardovscruuz <eduardovscruuz@gmail.com>
2023-03-15 20:40:06 -0300

    connected to users app

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { request, response } = require("express");
 const express = require("express");
 const uuid = require("uuid");
+const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 app.use(express.json());
+app.use(cors());
 
 const users = [];
 
